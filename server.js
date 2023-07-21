553cb4f1f7f87d40dd178aceb6c44cf2ebda69bc
DLSYu <hanz_yu@dlsu.edu.ph>
2023-07-21 16:47:31 +0800

    first post?
    maybe testing?

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./db");
+const { post } = require("./routes/profiles");
 const db = connectDB();
 
 app.set("view engine", "ejs");
@@ -63,6 +64,32 @@ app.get("/", (req, res) => {
     ];
 
     res.render("index", { topPostsText, posts, user: process.env.user });
+});
+
+app.get("/post/:postId", (req, res) => {
+    // Assuming you have a posts array with the individual post data
+    // Replace 'posts' below with your actual data source
+    
+    // const post = posts.find(post => post.link === req.params.postId);
+
+    // if (!post) {
+    //     // Handle the case when the post is not found (e.g., show a 404 page)
+    //     return res.status(404).render("notfound", { user: process.env.user });
+    // }
+
+    res.render("post", { pageTitle: "The Quick Brown Dog", 
+                         numberOfVotes: 5,
+                         postAuthor: "u/Govna",
+                         postDate: "12 hours ago",
+                         postCaption: "The Quick Brown Dog",
+                         postImageSrc: "/images/test_pics/posts/instadog.jpg",
+                         commentUsername: "u/DogsDBest",
+                         comment: {
+                            commenter: 'DoggoExpert', // Replace this with the username of the commenter for this comment
+                            commentDate: '1 hour ago', // Replace this with the date of the comment
+                            commentText: "That's not brown?!1!"// Add more properties as needed for the comment
+                        },
+                         user: process.env.user });
 });
 
 const profilesRoutes = require('./routes/profiles');
