f567d882d2cb08ba350df190a581a4e5a61e36a2
Milkz03 <mikkel_gamboa@dlsu.edu.ph>
2023-07-23 22:37:24 +0800

    search engine working
    need to fix the css for search

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const connectDB = require("./db/config/db");
 const { post } = require("./server/routes/profiles");
 const db = connectDB();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
