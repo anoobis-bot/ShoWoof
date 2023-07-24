c3bf1c5a7d2d851ac291ab1d7965c4ddd3488adc
Francis De Leon <business.francis300@gmail.com>
2023-07-24 19:34:18 +0800

    Upvote/downvote on profille

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -9,6 +9,9 @@ router.get('/:username', async (req, res) => {
     const postData = await Post.find({"author": req.username});
     const profileData = await Profile.findOne({"username": req.username});
 
+    const userDoc = await Profile.findOne({"username": process.env.user});
+    const userId = userDoc._id;
+
     try {
         const profilePic = profileData.profilePicture;
         const backgroundPic = profileData.backgroundPicture;
@@ -17,7 +20,8 @@ router.get('/:username', async (req, res) => {
             username: req.username,
             data: postData,
             pPicPath: profilePic,
-            bgPicPath: backgroundPic});
+            bgPicPath: backgroundPic,
+            userID: userId});
     } catch (err) {
         console.log(err);
     }
