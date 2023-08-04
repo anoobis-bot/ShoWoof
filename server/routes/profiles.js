cc70cf24295900cf778424043f655eea345783a0
Francis De Leon <business.francis300@gmail.com>
2023-08-04 18:39:58 +0800

    input fields the current user information in edit

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -9,7 +9,7 @@ router.get('/:username', async (req, res) => {
 });
 
 router.get('/:username/edit', (req, res) => {
-    res.render("edit_profile", { user: req.user.username })
+    res.render("edit_profile", { user: req.user })
 });
 
 router.param("username", (req, res, next, username) => {
