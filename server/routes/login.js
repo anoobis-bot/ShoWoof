6cfaa498fe94e085f3978563ad20e0fcfe8bbb78
Francis De Leon <business.francis300@gmail.com>
2023-08-04 16:40:35 +0800

    created logout function

--- server/routes/login.js
+++ server/routes/login.js
@@ -27,4 +27,11 @@ router.post('/login', auth.checkAlreadyAuthenticated, passport.authenticate('loc
     failureFlash: true,
 }))
 
+router.delete('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+});
+
 module.exports = router;
