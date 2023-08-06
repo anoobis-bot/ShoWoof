db8a4377c2620f72ec8e81b9221a162a5b9ecd01
Francis De Leon <business.francis300@gmail.com>
2023-08-07 00:00:36 +0800

    debug

--- controller/profiles_controller.js
+++ controller/profiles_controller.js
@@ -54,6 +54,7 @@ async function updateUser(req_body, req_files) {
     }
 
     if (req_files['profile']) {
+        console.log("The path to picture is: " + req_files['profile'][0].path);
         await Profile.updateOne({"username": req_body.currentUser}, {$set:{"profilePicture": '/' + req_files['profile'][0].path.split('\\').slice(1).join('/')}});
     }
     
