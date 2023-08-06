d5a2a25303a012fc81d1b4e8ff4b60f859003e7c
Francis De Leon <business.francis300@gmail.com>
2023-08-06 17:07:20 +0800

    made small adjustments on edit profile where users cannot change to existing username/email

--- public/js/edit_profile.js
+++ public/js/edit_profile.js
@@ -1,4 +1,63 @@
 $(document).ready(function() {
+    const usernameInput = document.querySelector("#username_change");
+    const editButton = document.querySelector("#submit-button");
+    const errorMessage = document.querySelector("#error-msg");
+
+    const currentUser = document.querySelector("#currentUser").value;
+    const currentEmail = document.querySelector("currentEmail").value;
+
+    const emailInput = document.querySelector("#email");
+
+    usernameInput.addEventListener("keyup", async function (e) {
+        console.log(currentUser)
+        if (usernameInput.value !== currentUser) {
+            try {
+                const response = await (await fetch('/profiles/checkUsernameExist/' + usernameInput.value, {
+                    method: "GET",
+                })).json();
+                
+                if (response.valid !== true) {
+                    editButton.disabled = true;
+                    errorMessage.innerHTML = "Username already exist!";
+                    usernameInput.style.backgroundColor = "Pink";
+                }
+                else {
+                    editButton.disabled = false;
+                    errorMessage.innerHTML = "";
+                    usernameInput.style.backgroundColor = "white";
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        
+        
+    });
+
+    emailInput.addEventListener("keyup", async function (e) {
+        if (emailInput.value !== currentEmail) {
+            try {
+                const response = await (await fetch('/profiles/checkEmailExist/' + emailInput.value, {
+                    method: "GET",
+                })).json();
+                
+                if (response.valid !== true) {
+                    editButton.disabled = true;
+                    errorMessage.innerHTML = "Username already exist!";
+                    emailInput.style.backgroundColor = "Pink";
+                }
+                else {
+                    editButton.disabled = false;
+                    errorMessage.innerHTML = "";
+                    emailInput.style.backgroundColor = "white";
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    });
+
+
     $("#submit-button").click((e) => {
         let new_username = $("input#username_change").val();
         let new_email = $("input#email").val();
